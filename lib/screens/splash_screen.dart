2fef945796b514b006f9f04c922c0a635c6e4f55
Arif <faizin.arief@gmail.com>
2023-03-16 13:21:35 +0700

    deleted unused file

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
